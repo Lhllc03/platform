d4ce93325a297170f3c318ac8e350d2f7a8b7002
Bruno Babic <bruno.babic@shoutem.com>
2017-11-14 15:28:23 +0100

    Merge master

--- ios/ShoutemApp/AppDelegate.m
+++ ios/ShoutemApp/AppDelegate.m
@@ -30,9 +30,6 @@
     [audioSession setCategory:AVAudioSessionCategoryPlayback
                         error:&setCategoryError];
 
-  // Appetizer.io params check
-  NSDictionary *initialProperties = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"initialProps"];
-
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
